7c7f66a406ea78b07211050d00d773db2e871e31
nagadeepakbhushan <nagadeepak000@gmail.com>
2021-03-02 13:38:10 +0530

    intial-commit

---
+++ server/quinbayrailways/src/main/java/com/quinbay/quinbayrailways/QuinbayrailwaysApplication.java
@@ -0,0 +1,13 @@
+package com.quinbay.quinbayrailways;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuinbayrailwaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuinbayrailwaysApplication.class, args);
+	}
+
+}
---
+++ server/quinbayrailways/src/test/java/com/quinbay/quinbayrailways/QuinbayrailwaysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quinbay.quinbayrailways;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuinbayrailwaysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
